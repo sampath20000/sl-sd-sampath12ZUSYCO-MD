5823c5161d8991a0ff4cd8004297d6184c007a83
sampath20000 <159634233+sampath20000@users.noreply.github.com>
2024-02-24 21:27:09 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -16,9 +16,9 @@ Creator : @darkmakerofc , @mr-nima-x
 */
 
 module.exports = {
-  OWNER_NUMBER: "94715166712",
-  SUDO_NUMBERS : "94715166712,94719574492",
-  OWNER_NAME: "『 𝙼𝚁 𝙽𝙸𝙼𝙰 』",
+  OWNER_NUMBER: "94778529895",
+  SUDO_NUMBERS : "94778529895",
+  OWNER_NAME: "『 𝙼𝚁 sl sampath 』",
   BOT_NAME : "ＺＵＳＹＣＯ",
   ZUSYCO_FILE : "ZUSYCO-QR",
   LOGOS: {
